0e9afc00b1c7b3bcf6d53a28b5e1cc9b134eb802
Marc Nicdao <nicdaom46^@gmail.com>
2020-08-02 20:51:48 -0700

    added header and footer, and function files

---
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/footer.php
@@ -0,0 +1,6 @@
+<p>This is the footer</p>
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
--- fictional-university/app/public/wp-content/themes/fictional-university-theme/index.php
+++ fictional-university/app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,9 +1,11 @@
-<?php
+<?php get_header();
 
-  $myNames = array("Phoebe", "Lucy", 'Pia', 'Logan');
-  $count = 0 ;
-  while($count < count($myNames)) {
-    echo "<li>Hi, my name is $myNames[$count].</li>";
-    $count++;
-  }
+  while(have_posts()) {
+    the_post(); ?>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php the_content();?>
+    <hr/>
+
+  <?php }
+    get_footer();
 ?>
